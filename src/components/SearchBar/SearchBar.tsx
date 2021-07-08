300923760c2585eafe9086f00a27b94998d475a8
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2021-07-08 20:07:10 +0300

    Searchbar position & style change

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,15 @@
-import './styles.css';
+import "./styles.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faBackspace } from "@fortawesome/free-solid-svg-icons";
 
 function SearchBar() {
   return (
     <div className="search-bar">
       <input type="text" placeholder="Искать здесь..."></input>
       <button type="submit"></button>
-      <div className="delete">X</div>
+      <FontAwesomeIcon icon={faBackspace} />
+      <FontAwesomeIcon icon={faSearch} />
     </div>
   );
 }
