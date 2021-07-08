d77158d4ce53827aa072f39695bf00a80ee9aac6
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2021-07-08 21:28:43 +0300

    Update SearchBar placeholder

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -6,7 +6,7 @@ import { faBackspace } from "@fortawesome/free-solid-svg-icons";
 function SearchBar() {
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Искать здесь..."></input>
+      <input type="text" placeholder="Search..."></input>
       <button type="submit"></button>
       <FontAwesomeIcon icon={faBackspace} />
       <FontAwesomeIcon icon={faSearch} />
