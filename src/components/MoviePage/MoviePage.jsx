127866d35eac6c2b96d10edd09d8db0295cab415
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2021-07-25 21:40:59 +0300

    Pages button styled & delete App.css class

--- src/components/MoviePage/MoviePage.jsx
+++ src/components/MoviePage/MoviePage.jsx
@@ -1,28 +1,23 @@
-import React, { useContext } from "react";
-import MovieList from '../MovieList/MovieList';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import "./styles.scss";
 
-const MoviePage = ({pageNumber, setPageNumber}) => {
-
+const MoviePage = ({ pageNumber, setPageNumber }) => {
   return (
     <div className="movie-pages">
-      {pageNumber > 1 &&
-        <button
-          className="previousPage"
+      {pageNumber > 1 && (
+        <FontAwesomeIcon
+          className="pages-icon"
+          icon={faArrowLeft}
           onClick={() => setPageNumber(pageNumber - 1)}
-        >
-          Prev Page
-        </button>
-      }
+        />
+      )}
 
-      <button
-      onClick={() => setPageNumber(pageNumber + 1)}
-      >
-        Next Page
-      </button>
-
-      <faArrowRight/>
+      <FontAwesomeIcon
+        className="pages-icon"
+        icon={faArrowRight}
+        onClick={() => setPageNumber(pageNumber + 1)}
+      />
     </div>
   );
 };
