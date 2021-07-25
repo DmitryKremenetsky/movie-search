e3b034052fa070f9fb9e9c64a58c298e088c5db4
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2021-07-25 15:37:23 +0300

    Add new import

--- src/components/MoviePage/MoviePage.jsx
+++ src/components/MoviePage/MoviePage.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { MovieList } from '../../components/MovieList/MovieList';
 
 const MoviePage = () => {
   const { pageNumber , showPage } = useContext(MovieList);
