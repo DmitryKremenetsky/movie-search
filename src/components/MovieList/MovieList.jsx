9cb185996285f01b0fdcd143aad7116d39bece99
Kirill Kolchanov <62650256+KirillKolchanov@users.noreply.github.com>
2021-07-25 18:39:43 +0200

    Edit logic for MoviePage (#16)

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,22 +1,13 @@
 import { useEffect, useState } from "react";
 import "./styles.scss";
 
-const MovieList = ({ searchQuery, movies, setMovies }) => {
+const MovieList = ({ searchQuery, movies, setMovies, pageNumber }) => {
   const [isLoading, setIsLoading] = useState(false);
 
-  const getMovieRequest = async (searchQuery, pageNumber = 1) => {
+  const getMovieRequest = async (searchQuery) => {
     const url = `http://www.omdbapi.com/?&apikey=35def29d&s=${searchQuery}&page=${pageNumber}`;
     const response = await fetch(url);
     return await response.json();
-  };
-
-  const pageNumber = (direction, setCurrentPage) => {
-    if(direction === "next") {
-      setCurrentPage(pageNumber => pageNumber + 1);
-      setIsLoading(true);
-    } else if (direction === "previous" && pageNumber !== 1) {
-      setCurrentPage(pageNumber => pageNumber - 1);
-    }
   };
 
   useEffect(() => {
@@ -31,7 +22,7 @@ const MovieList = ({ searchQuery, movies, setMovies }) => {
       })
 
       .catch(() => setIsLoading(false));
-  }, [searchQuery, setMovies]);
+  }, [searchQuery, pageNumber]);
 
   if (isLoading) return <div className="loading">Loading...</div>;
 
