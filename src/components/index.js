e1b3fa300e7aa1e88d57515ce8a7d3cd0a20f7ba
Dmitry Kremenetsky <73497019+DmitryKremenetsky@users.noreply.github.com>
2021-07-26 19:56:00 +0300

    Revert "Fix onClear logic & styles fix"
    This reverts commit e95c352715bd49f5b7332035b3ba0878bf4912ee.

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import SearchBar from "./SearchBar/SearchBar";
 import MovieList from "./MovieList/MovieList";
 import MoviePage from "./MoviePage/MoviePage";
-import HeaderTitle from "./HeaderTitle/HeaderTitle";
 
 export {
   SearchBar,
   MovieList,
   MoviePage,
-  HeaderTitle,
 };
