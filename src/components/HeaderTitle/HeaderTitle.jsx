e95c352715bd49f5b7332035b3ba0878bf4912ee
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2021-07-26 19:47:45 +0300

    Fix onClear logic & styles fix

---
+++ src/components/HeaderTitle/HeaderTitle.jsx
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+const HeaderTitle = () => {
+  return (
+    <header className="header">
+        <h1 className="title">MovieSearchApp</h1>
+    </header>
+  );
+};
+
+export default HeaderTitle;
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import SearchBar from "./SearchBar/SearchBar";
 import MovieList from "./MovieList/MovieList";
 import MoviePage from "./MoviePage/MoviePage";
+import HeaderTitle from "./HeaderTitle/HeaderTitle";
 
 export {
   SearchBar,
   MovieList,
   MoviePage,
+  HeaderTitle,
 };
