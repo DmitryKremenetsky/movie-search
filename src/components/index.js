5368d0ff8a53f5463f7f49c88991f7bfda4af5a0
Dmitry Kremenetsky <73497019+DmitryKremenetsky@users.noreply.github.com>
2021-07-26 20:24:29 +0300

    Add HeaderTitle component (#24)
    * Add HeaderTitle component
    
    * Add method setMovies

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import SearchBar from "./SearchBar/SearchBar";
 import MovieList from "./MovieList/MovieList";
 import MoviePage from "./MoviePage/MoviePage";
+import HeaderTitle from "./HeaderTittle/HeaderTittle";
 
 export {
   SearchBar,
   MovieList,
   MoviePage,
+  HeaderTitle,
 };
