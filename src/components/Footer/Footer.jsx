9577c52c9e6aa3c59942fd104b3dd679ce4b7fdb
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2021-07-27 12:04:10 +0200

    Change colors in HeaderTutle & Add Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import "./styles.scss";
+
+const Footer = () => {
+  return (
+
+  <footer class="site-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-sm-6 col-xs-12">
+          <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by
+        <a href="#">    Dmitry & Kirill</a>.
+          </p>
+        </div>
+
+        <div class="col-md-4 col-sm-6 col-xs-12">
+          <ul class="social-icons">
+            <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a class="instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+            <li><a class="youtube" href="#"><i class="fa fa-youtube"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  );
+};
+
+export default Footer;
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import SearchBar from "./SearchBar/SearchBar";
 import MovieList from "./MovieList/MovieList";
 import MoviePage from "./MoviePage/MoviePage";
 import HeaderTitle from "./HeaderTittle/HeaderTittle";
+import Footer from "./Footer/Footer";
 
 export {
   SearchBar,
   MovieList,
   MoviePage,
   HeaderTitle,
+  Footer,
 };
